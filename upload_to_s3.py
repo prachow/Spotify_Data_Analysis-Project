7d9127aa31c8d18b9b3e74b244a970622660d700
Prathima Chowdary Sudalagunta <59141264+prachow@users.noreply.github.com>
2025-04-18 15:21:09 -0500

    Update upload_to_s3.py

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,8 +1,8 @@
 import boto3 # type: ignore
 
 # Replace with your actual AWS credentials
-aws_access_key = "AKIA4PFS2D4UEM3RQF77"
-aws_secret_key = "idz81zQI8McyeuI6QE4kdKNqdvBNWHSyx92emjvF"
+aws_access_key = "*******************"
+aws_secret_key = "*************************************************************"
 region_name = "us-east-1"  # or your desired region
 
 # Create S3 client with credentials
@@ -21,6 +21,6 @@ s3_key = 'spotify-data/spotify-2023.csv'
 # Upload
 try:
     s3.upload_file(file_path, bucket_name, s3_key)
-    print(f"✅ Upload successful! File available at: s3://{bucket_name}/{s3_key}")
+    print(f"Upload successful! File available at: s3://{bucket_name}/{s3_key}")
 except Exception as e:
-    print(f"❌ Upload failed: {e}")
+    print(f"Upload failed: {e}")
