d8e0be64c65b5b069c6844eca0b04a1534683bac
Prathima Chowdary Sudalagunta <59141264+prachow@users.noreply.github.com>
2025-04-23 13:17:58 -0500

    Update upload_script.py

--- upload_script.py
+++ upload_script.py
@@ -3,7 +3,7 @@ import boto3
 s3 = boto3.client('s3')
 
 # Replace with your actual bucket name and path
-bucket_name = 'spotifydataa'  # e.g., 'spotify-project-data'
+bucket_name = 'spotifydataa'  
 script_path_in_s3 = 'scripts/spotify_transform.py'
 
 # Upload local script to S3
